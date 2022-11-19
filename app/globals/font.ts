ada46c0b48450a976db29b5d8b97d31e61d11731
Ayanfe <ibitoyeayanfeoluwa@gmail.com>
2022-11-19 18:20:04 +0100

    fix: add subsets

--- app/globals/font.ts
+++ app/globals/font.ts
@@ -2,8 +2,10 @@ import { Lato } from "@next/font/google";
 
 export const LATO_REGULAR = Lato({
 	weight: "400",
+	subsets: ["latin"],
 });
 
 export const LATO_BOLD = Lato({
 	weight: "700",
+	subsets: ["latin"],
 });
