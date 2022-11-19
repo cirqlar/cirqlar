abcfac2a5dc2a211aae4fcff0dde1509bbd1eef4
Ayanfe <ibitoyeayanfeoluwa@gmail.com>
2022-11-19 18:14:31 +0100

    fix: some accessibility issues

--- app/components/social-links.tsx
+++ app/components/social-links.tsx
@@ -6,9 +6,9 @@ const SocialLinks = () => {
 	return (
 		<div className="flex justify-center">
 			<div className="flex gap-3 p-2 w-min justify-center items-center border border-white rounded-md">
-				<a href="https://github.com/cirqlar" target="_blank" rel="noreferrer"><GithubIcon /></a>
-				<a href="https://www.linkedin.com/in/ibitoyeayanfe/" target="_blank" rel="noreferrer"><LinkedinIcon /></a>
-				<a href="mailto:ibitoyeayanfeoluwa@gmail.com" target="_blank" rel="noreferrer"><EmailIcon /></a>
+				<a href="https://github.com/cirqlar" target="_blank" rel="noreferrer" aria-label="Check out my Github"><GithubIcon /></a>
+				<a href="https://www.linkedin.com/in/ibitoyeayanfe/" target="_blank" rel="noreferrer" aria-label="Check out my Linkedin"><LinkedinIcon /></a>
+				<a href="mailto:ibitoyeayanfeoluwa@gmail.com" target="_blank" rel="noreferrer" aria-label="Send me an Email"><EmailIcon /></a>
 			</div>
 		</div>
 	);
--- app/index/other.tsx
+++ app/index/other.tsx
@@ -7,7 +7,7 @@ const OtherSection = () => {
 			<div className="w-[480px] relative h-full mx-auto flex flex-col justify-end items-center pb-28">
 				<h2 className={`${LATO_BOLD.className} text-8xl mb-5`}>Other</h2>
 				<p className="text-xl text-justify mb-5">I'm learning 3d using Blender and Unreal Engine 5 and hope to show my progress.</p>
-				<p className="flex p-2 w-auto justify-center items-center border border-[#777777] text-[#777777] rounded-md">Coming Soon</p>
+				<p className="flex p-2 w-auto justify-center items-center border border-gray-400 text-gray-400 rounded-md">Coming Soon</p>
 			</div>
 		</section>
 	);
