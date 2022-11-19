7a52abf4dfb7cdef57445d3e1397f6ac6db4fb6e
Ayanfe <ibitoyeayanfeoluwa@gmail.com>
2022-11-19 16:19:50 +0100

    chore: final commit before changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,14 @@ export default function Home() {
   return (
     <>
       <main className={`${styles.indexGrid} bg-primary`}>
-        <div className={`${styles.main} bg-dark`}>
-          <h1 className={MERRI_REGULAR.className}>Àyànfẹ</h1>
-          <p>I'm a frontend developer mostly working with frontend frameworks and tools including (but not limited to) React, Nextjs and Vuejs. I'm also learing 3d.</p>
+        <div className={`${styles.main} bg-dark flex flex-col justify-center items-start md:px-10 lg:px-24`}>
+          <div className="md:w-[320px] lg:w-[410px]">
+            <h1>
+              <span className="lg:text-4xl">Hi! It's me!</span>
+              <span className={`${MERRI_REGULAR.className} pt-2 pb-6 flex items-center md:text-8xl md:h-[160px] lg:text-9xl lg:h-[220px]`}>Àyànfẹ</span>
+            </h1>
+            <p className="md:text-base lg:text-xl text-justify">I'm a frontend developer mostly working with frontend frameworks and tools including (but not limited to) React, Nextjs and Vuejs. I'm also learing 3d.</p>
+          </div>
         </div>
         <div className={`${styles.fCont} relative flex justify-center items-center`}>
           <BgImage fill className="object-cover" src={img} alt="" sizes={`(max-width: 768px) 63%, 50%`}>
