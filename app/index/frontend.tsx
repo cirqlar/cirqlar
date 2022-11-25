282b1a026b90c00ecd243f9205b628f9925e35b6
Ayanfe <ibitoyeayanfeoluwa@gmail.com>
2022-11-25 14:42:19 +0100

    feat: frontend bg

--- app/index/frontend.tsx
+++ app/index/frontend.tsx
@@ -3,8 +3,31 @@ import { LATO_BOLD } from "../globals/font";
 
 const FrontendSection = () => {
 	return (
-		<section id="frontend">
-			<div></div>
+		<section id="frontend" className="relative">
+			<div className="absolute inset-0 p-8 pb-16 flex justify-center items-center">
+				<div className="w-full max-w-5xl aspect-video bg-[#2C2C3E] rounded-2xl grid grid-rows-[80px_1fr] grid-cols-1 overflow-hidden">
+					<div className="grid grid-rows-2 grid-cols-1">
+						<div className="flex justify-between items-center">
+							<div className="h-full flex flex-row-reverse">
+								<div className="h-full w-40 rounded-tr-xl bg-[#393951] -ml-2 overflow-hidden"></div>
+								<div className="h-full w-40 rounded-tr-xl bg-[#484862] -ml-2 overflow-hidden"></div>
+								<div className="h-full w-40 rounded-tr-xl bg-[#2E1760] overflow-hidden"></div>
+							</div>
+							<div className="flex gap-4 justify-center items-center w-min pr-3">
+								<div className="w-3 h-3 rounded-full bg-green-600"></div>
+								<div className="w-3 h-3 rounded-full bg-orange-600"></div>
+								<div className="w-3 h-3 rounded-full bg-red-600"></div>
+							</div>
+						</div>
+						<div className="bg-[#181831] px-2 py-3">
+							<div className="w-3/4 h-full bg-[#484862]"></div>
+						</div>
+					</div>
+					<div className="min-h-0">
+
+					</div>
+				</div>
+			</div>
 			<div className="w-min relative h-full mx-auto flex flex-col justify-center">
 				<h2 className={`${LATO_BOLD.className} text-8xl mb-8`}>Frontend Development</h2>
 				<nav className="text-2xl flex justify-center items-center gap-9">
