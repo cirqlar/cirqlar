7793abac619ef2deb48730f24e10b145c81d08a9
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2025-01-10 14:37:50 +0100

    feat: flipping menu

--- app/(nested)/client_main.tsx
+++ app/(nested)/client_main.tsx
@@ -3,12 +3,13 @@
 import { useEffect, useState } from "react";
 
 export default function Main(props: { children?: React.ReactNode }) {
-	const [open, setOpen] = useState((window ?? {})?.matchMedia('(prefers-reduced-motion)').matches ?? false);
+	const [open, setOpen] = useState((typeof window === 'undefined' ? {} as Window : window)?.matchMedia?.('(prefers-reduced-motion)').matches ?? false);
 
 	useEffect(() => setOpen(true), []);
 
 	return <main className={`
 		main w-full h-full p-10 md:p-16
+		group/client
 		${open ? 'open' : ''} 
 		${open ? 'bg-transparent' : 'bg-slate-700'}
 	`}>{props.children}</main>
