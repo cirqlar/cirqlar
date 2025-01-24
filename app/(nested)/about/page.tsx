606eb6d151faa22165bf33ebf64e16492755cee3
Ayanfe Ibitoye <ibitoyeayanfeoluwa@gmail.com>
2025-01-24 20:13:33 +0100

    fix: style mobile about better

--- app/(nested)/about/page.tsx
+++ app/(nested)/about/page.tsx
@@ -1,10 +1,11 @@
+import { LinkInternal } from "@/components/links";
 import styles from "./about.module.css";
 
 export default function AboutPage() {
 	return (
-		<div>
+		<div className="">
 			<div
-				className={`absolute left-0 flex w-full flex-col items-center justify-center px-10 md:flex-row md:px-20 ${styles.container} `}
+				className={`left-0 flex w-full flex-col items-center md:absolute md:flex-row md:justify-center md:px-20 ${styles.container} `}
 			>
 				<h2 className={`text-5xl md:text-6xl ${styles.title}`}>
 					About
@@ -16,16 +17,32 @@ export default function AboutPage() {
 					<div className={styles.divider}></div>
 
 					<div className={styles.text_container}>
-						<p className="text-type">
-							Lorem ipsum dolor sit amet consectetur adipisicing
-							elit. Reiciendis harum perspiciatis aliquam
-							aspernatur provident enim quasi.
+						<p className="text-pretty">
+							Hello. I'm a Software Developer. I've been employed
+							in Frontend development for about{" "}
+							<LinkInternal
+								href="/webdev"
+								className="underline underline-offset-4"
+							>
+								5 years
+							</LinkInternal>{" "}
+							and I've been programming for much longer.
 						</p>
-						<p className="text-type mt-2 text-pretty">
-							Exercitationem, doloremque sint modi architecto
-							provident accusantium libero illo est nihil
-							incidunt. Quidem dolorem numquam tempora debitis,
-							iure odio in praesentium molestias.
+						<p className="mt-2 text-pretty">
+							I've got tons of professional experience using
+							frontend and frontend-related tech like Typescript,
+							React, Vue, Tailwind and I've worked with{" "}
+							<LinkInternal
+								href="/webdev"
+								className="underline underline-offset-4"
+							>
+								a lot more
+							</LinkInternal>{" "}
+							in my free time.
+						</p>
+						<p className="mt-2 text-pretty">
+							I love thinking through problems, working on useful
+							software and learning new technology.
 						</p>
 					</div>
 				</div>
