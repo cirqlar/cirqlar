b680286376954dffa1e1cd37af561ab89d160742
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2025-06-01 16:55:40 +0100

    fix: add crossorigin attribute to preloads

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,18 +48,21 @@ export default function RootLayout({
 					href="/alower.svg"
 					as="image"
 					type="image/svg+xml"
+					crossOrigin="anonymous"
 				/>
 				<link
 					rel="preload"
 					href="/aupper.svg"
 					as="image"
 					type="image/svg+xml"
+					crossOrigin="anonymous"
 				/>
 				<link
 					rel="preload"
 					href="/iupper.svg"
 					as="image"
 					type="image/svg+xml"
+					crossOrigin="anonymous"
 				/>
 			</head>
 			<body className={`${lato.className} bg-back text-type antialiased`}>
